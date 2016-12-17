8bbc74b02405959996fe11eb19cf7752f6df0e0d
Hervé BREDIN <bredin@limsi.fr>
2016-12-17 21:15:51 +0100

    improve: change signature of preprocessor.__call__

--- pyannote/database/protocol/speaker_diarization.py
+++ pyannote/database/protocol/speaker_diarization.py
@@ -38,7 +38,7 @@ class SpeakerDiarizationProtocol(Protocol):
     ----------
     preprocessors : dict or (key, preprocessor) iterable
         When provided, each protocol item (dictionary) are preprocessed, such
-        that item[key] = preprocessor(**item). In case 'preprocessor' is not
+        that item[key] = preprocessor(item). In case 'preprocessor' is not
         callable, it should be a string containing placeholder for item keys
         (e.g. {'wav': '/path/to/{uri}.wav'})
     """
