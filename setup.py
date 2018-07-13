94c8729d789e7a0abc711baee601b83c7d99cdd0
Hervé Bredin <bredin@limsi.fr>
2018-07-13 12:41:48 +0200

    feat: drop support for Python < 3.6

--- setup.py
+++ setup.py
@@ -54,8 +54,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: Scientific/Engineering"
     ],
