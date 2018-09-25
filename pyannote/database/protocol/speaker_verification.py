a98822ac7e2ad1dda985ca602e990ab5fa6cf2e8
Hervé Bredin <bredin@limsi.fr>
2018-09-25 11:35:05 +0200

    BREAKING: update SpeakerVerificationProtocol
    * now inherits from SpeakerDiarizationProtocol
    * now has {subset}_trial methods yielding trials as dictionaries with  'reference', 'file1', and 'file2' keys

--- pyannote/database/protocol/speaker_verification.py
+++ pyannote/database/protocol/speaker_verification.py
@@ -3,7 +3,7 @@
 
 # The MIT License (MIT)
 
-# Copyright (c) 2017 CNRS
+# Copyright (c) 2017-2018 CNRS
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -27,17 +27,125 @@
 # Hervé BREDIN - http://herve.niderb.fr
 
 
-from .speaker_spotting import SpeakerSpottingProtocol
+from .speaker_diarization import SpeakerDiarizationProtocol
 
-class SpeakerVerificationProtocol(SpeakerSpottingProtocol):
+
+class SpeakerVerificationProtocol(SpeakerDiarizationProtocol):
     """Speaker verification protocol
 
     Parameters
     ----------
     preprocessors : dict or (key, preprocessor) iterable
         When provided, each protocol item (dictionary) are preprocessed, such
-        that item[key] = preprocessor(item). In case 'preprocessor' is not
-        callable, it should be a string containing placeholder for item keys
-        (e.g. {'audio': '/path/to/{uri}.wav'})
+        that item[key] = preprocessor(item).
     """
-    pass
+
+    def trn_try_iter(self):
+        for trial in []:
+            yield trial
+
+    def train_trial(self):
+        """Iterate over the trials of the train set
+
+        Each trial is yielded as a dictionary with the following keys:
+
+        ['reference'] (`boolean`)
+            Groundtruth: True for a target trial, False for a non-target trial.
+
+        ['file{1|2}'] (`dict`)
+            Both parts of the trial are provided as dictionaries with the
+            following keys (as well as keys added by preprocessors):
+
+            ['uri'] (`str`)
+                Unique file identifier.
+
+            ['try_with'] (`pyannote.core.{Segment|Timeline}`), optional
+                Part(s) of the file to use in the trial. Default is to use the
+                whole file.
+        """
+
+        generator = self.trn_try_iter()
+
+        if self.progress:
+            generator = tqdm(
+                generator, desc='Trial (train set)',
+                total=getattr(self.trn_try_iter, 'n_items', None))
+
+        for current_trial in generator:
+            current_trial['file1'] = self.preprocess(current_trial['file1'])
+            current_trial['file2'] = self.preprocess(current_trial['file2'])
+            yield current_trial
+
+    def dev_try_iter(self):
+        for trial in []:
+            yield trial
+
+    def development_trial(self):
+        """Iterate over the trials of the development set
+
+        Each trial is yielded as a dictionary with the following keys:
+
+        [`reference`] (`boolean`)
+            Groundtruth: True for a target trial, False for a non-target trial.
+
+        [`file{1|2}`] (`dict`)
+            Both parts of the trial are provided as dictionaries with the
+            following keys (as well as keys added by preprocessors):
+
+            [`uri`] (`str`)
+                Unique file identifier.
+
+            [`try_with`] (`pyannote.core.{Segment|Timeline}`), optional
+                Part(s) of the file to use in the trial. Default is to use the
+                whole file.
+
+        """
+
+        generator = self.dev_try_iter()
+
+        if self.progress:
+            generator = tqdm(
+                generator, desc='Trial (development set)',
+                total=getattr(self.dev_try_iter, 'n_items', None))
+
+        for current_trial in generator:
+            current_trial['file1'] = self.preprocess(current_trial['file1'])
+            current_trial['file2'] = self.preprocess(current_trial['file2'])
+            yield current_trial
+
+    def tst_try_iter(self):
+        for trial in []:
+            yield trial
+
+    def test_trial(self):
+        """Iterate over the trials of the test set
+
+        Each trial is yielded as a dictionary with the following keys:
+
+        [`reference`] (`boolean`)
+            Groundtruth: True for a target trial, False for a non-target trial.
+
+        [`file{1|2}`] (`dict`)
+            Both parts of the trial are provided as dictionaries with the
+            following keys (as well as keys added by preprocessors):
+
+            [`uri`] (`str`)
+                Unique file identifier.
+
+            [`try_with`] (`pyannote.core.{Segment|Timeline}`), optional
+                Part(s) of the file to use in the trial. Default is to use the
+                whole file.
+
+        """
+
+        generator = self.tst_try_iter()
+
+        if self.progress:
+            generator = tqdm(
+                generator, desc='Trial (test set)',
+                total=getattr(self.tst_try_iter, 'n_items', None))
+
+        for current_trial in generator:
+            current_trial['file1'] = self.preprocess(current_trial['file1'])
+            current_trial['file2'] = self.preprocess(current_trial['file2'])
+            yield current_trial
