4325ecf20db6acb0619474752bf26aeb91f26a14
Hervé BREDIN <hbredin@users.noreply.github.com>
2023-04-21 10:42:58 +0200

    improve: do not complain about missing scope for meta protocols

--- pyannote/database/custom.py
+++ pyannote/database/custom.py
@@ -442,16 +442,18 @@ def create_protocol(
             metadata["classes"] = protocol_entries.pop("classes")
 
     if issubclass(base_class, SpeakerDiarizationProtocol):
-        if "scope" in protocol_entries:
-            scope = protocol_entries.pop("scope")
-        else:
-            scope = "file"
+        scope = protocol_entries.pop("scope", None)
+
+        if scope is None and database != "X":
             msg = (
                 f"'{database}.{task}.{protocol}' found in {database_yml} does not define "
                 f"the 'scope' of speaker labels (file, database, or global). Setting it to 'file'."
             )   
             print(msg)
-        metadata["scope"] = scope
+            metadata["scope"] = "file"
+
+        else:
+            metadata["scope"] = scope
 
     methods = dict()
     for subset, subset_entries in protocol_entries.items():
