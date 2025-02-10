c23aadc13e62327af40146f410454ed361495f81
Hervé BREDIN <hbredin@users.noreply.github.com>
2025-02-10 08:57:19 +0100

    BREAKING: switch to uv, namespace package and Python 3.10+ (#109)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,8 +40,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'matplotlib.sphinxext.plot_directive',
-    'IPython.sphinxext.ipython_directive']
+    ]
 
 
 # Napoleon settings
@@ -65,7 +64,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = {'.rst': 'restructuredtext'}
 
 # The master toctree document.
 master_doc = 'index'
@@ -90,7 +89,7 @@ release = pyannote.database.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -109,9 +108,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -183,15 +180,5 @@ texinfo_documents = [
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/', None),
-    'pyannote.core': ('https://pyannote.github.com/pyannote-core', None),
+    'pyannote.core': ('https://pyannote.github.io/pyannote-core', None),
 }
-
-plot_pre_code = """import matplotlib.pyplot as plt
-from pyannote.core import notebook
-from pyannote.core import Segment
-from pyannote.core import Timeline
-from pyannote.core import Annotation
-from pyannote.core import SlidingWindow
-from pyannote.core import SlidingWindowFeature"""
-
-ipython_savefig_dir = '../../build/html/_static'
