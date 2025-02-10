3dca6ce2c13c759fa83232691f854358d78331d5
hbredin <hbredin@users.noreply.github.com>
2025-02-10 08:00:25 +0000

    deploy: ae8d73e17583d197937884e3d11d7c24c1bdbdf4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,10 +1,13 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.1.3',
-    LANGUAGE: 'None',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '6.0.0rc2.dev0+gae8d73e.d20250210',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
